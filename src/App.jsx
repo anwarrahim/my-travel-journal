e62851c1f0d81add34a274250dd79c1641142f82
Anwar Rahim <anwargithub@gmail.com>
2025-03-19 14:56:55 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/Header'
+
+
+function App() {
+  
+  return (
+    <>
+     <h1> working or not</h1>
+     <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+
+function Header(){
+return (
+    <>
+
+        <p>this is header</p>
+        <p>is it working or not</p>
+
+    </>
+)
+}
+
+export default Header
